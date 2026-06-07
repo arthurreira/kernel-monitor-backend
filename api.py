73410855cb3a826f21506bd1c9c2d0027bb3f03b
arthurreira <arthur.ferreiramiran@gmail.com>
2026-06-07 18:01:22 +0300

    feat: add /events , /stats, /health, endpoints + uptime + psutil

--- api.py
+++ api.py
@@ -3,6 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 import httpx
 from pydantic import BaseModel
+import subprocess
+import psutil
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -51,3 +54,42 @@ Answer concisely based on the data above."""
         )
     
     return {"answer": response.json()["response"]}
+
+@app.get("/events")
+async def get_events():
+    conn = sqlite3.connect('/home/naroshh/ebpf-lab/events.db')
+    rows = conn.execute('''
+        SELECT timestamp, pid, process, filename 
+        FROM events 
+        ORDER BY id DESC 
+        LIMIT 50
+    ''').fetchall()
+    conn.close()
+    return {"events": [
+        {"timestamp": r[0], "pid": r[1], "process": r[2], "filename": r[3]}
+        for r in rows
+    ]}
+
+@app.get("/stats")
+async def get_stats():
+    conn = sqlite3.connect('/home/naroshh/ebpf-lab/events.db')
+    total = conn.execute("SELECT COUNT() FROM events").fetchone()[0]
+    top = conn.execute("SELECT process, COUNT() as c FROM events GROUP BY process ORDER BY c DESC LIMIT 1").fetchone()
+    suspicious = conn.execute("SELECT COUNT(*) FROM events WHERE filename LIKE '%/etc/passwd%' OR filename LIKE '%/etc/shadow%'").fetchone()[0]
+
+    conn.close()
+    
+    uptime = subprocess.check_output(['uptime','-p']).decode().strip()
+
+    return {"total": total, "top_process": top[0], "suspicious": suspicious, "uptime": uptime}
+
+
+@app.get("/health")
+async def health():
+    return {
+        "cpu": psutil.cpu_percent(interval=0.1),
+        "ram_used": round(psutil.virtual_memory().used / 1e9, 1),
+        "ram_total": round(psutil.virtual_memory().total / 1e9, 1),
+        "model": "phi3:mini",
+        "backend": "172.20.10.2:8000"
+    }
